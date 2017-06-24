c22659ee951bbf6f62f18a5a47a197f58dba7cce
Remco de Wilde <remcodewilde@gmail.com>
2017-06-24 20:30:57 +0200

    Strings moved String resources hardcoded name moved String resources

--- app/src/main/java/com/teamtreehouse/interactivestory/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/MainActivity.java
@@ -1,6 +1,7 @@
 package com.teamtreehouse.interactivestory;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void startStory(String name) {
         Intent intent = new Intent(this, StoryActivity.class);
-        intent.putExtra("name", name);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key, name);
         startActivity(intent);
     }
 }
